90ae66673a9a0836a8c15d1ad24efeffbf8a655d
leegeunhyeok <lghlove0509@naver.com>
2019-02-12 22:25:24 +0900

    Add gulim font, script update

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin bash
+sudo apt-get update
+sudo apt-get -y install sed
 sudo apt-get -y install wget
 sudo apt-get -y install cabextract
 sudo apt-get -y install wine-stable
 export WINEARCH=win32
 export WINEPREFIX=~/.wine
-wine
+wine --version
 wineboot
 wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks -q --show-progress
 echo "Winetricks download success!"
@@ -15,5 +17,10 @@ echo "2. Install a Windows DLL or component CHECK!"
 echo "3. gdiplus, msxml6, riched30, wmp9 ALL CHECK!"
 rm -r winetricks
 
+echo "Copy korean font file to wine"
+cp ./font/gulim.tff ~/.wine/drive_c/windows/Fonts
+
 echo "Kakaotalk install executable file download"
 wget http://app.pc.kakao.com/talk/win32/KakaoTalk_Setup.exe -q --show-progress
+echo "Kakaotalk download success!"
+wine KakaoTalk_Setup.exe
