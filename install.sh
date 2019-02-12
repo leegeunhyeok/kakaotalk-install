b09f312f06ef916e9008ebac316a8d2aee86770f
leegeunhyeok <lghlove0509@naver.com>
2019-02-12 22:28:25 +0900

    Script update

--- install.sh
+++ install.sh
@@ -11,14 +11,14 @@ wineboot
 wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks -q --show-progress
 echo "Winetricks download success!"
 chmod 777 winetricks
-./winetricks
+sudo ./winetricks
 echo "1. Select the default wineprefix CHECK!"
 echo "2. Install a Windows DLL or component CHECK!"
 echo "3. gdiplus, msxml6, riched30, wmp9 ALL CHECK!"
 rm -r winetricks
 
 echo "Copy korean font file to wine"
-cp ./font/gulim.tff ~/.wine/drive_c/windows/Fonts
+cp ./font/gulim.ttf ~/.wine/drive_c/windows/Fonts
 
 echo "Kakaotalk install executable file download"
 wget http://app.pc.kakao.com/talk/win32/KakaoTalk_Setup.exe -q --show-progress
